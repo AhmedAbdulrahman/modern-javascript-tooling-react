4146a6637997d4ca22093d5cf059d25131844055
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-11-19 12:29:07 +0100

    ESLINT REACT Rules

--- src/index.js
+++ src/index.js
@@ -1,40 +1,19 @@
-import React from 'react'
+import * as React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-import Heading from 'components/Heading'
-import Checkout from 'containers/Checkout'
 
-const App = ({ title, query }) => {
+const App = React.forwardRef(function App(props, ref) {
+	const { children, ...other } = props
+
 	const [count, setCount] = React.useState(0)
 
-	React.useEffect(() => {
-		const getItem = () => {
-			fetch(`http://localhost:8080/${query}`)
-		}
-
-		getItem(query)
-	}, [query])
-
-	const handleCountUpdate = () => {
-		setCount(count + 1)
-	}
-
 	return (
-		<div>
-			<Heading>{title}</Heading>
-			<Checkout>This is Checkout container</Checkout>
-			<h1>Count is {count}</h1>
-			<button type='button' onClick={handleCountUpdate}>
-				Increment
-			</button>
+		<div ref={ref} {...other}>
+			This is React App {count}
+			<button onClick={() => setCount(count + 1)}>Increment</button>
 		</div>
 	)
-}
+})
 
-App.propTypes = {
-	title: PropTypes.string,
-	query: PropTypes.string,
-}
+export default App
 
-const root = document.querySelector('#root')
-ReactDOM.render(<App title='Hello JSX' onChange={() => console.log('here')} />, root)
+ReactDOM.render(<App />, document.getElementById('root'))
